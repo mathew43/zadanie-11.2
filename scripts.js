59e1f35dd0b0a87bb1c37e12f12e6f0d89b1c770
Mateusz Łukowski <mateusz.lukowski@wp.pl>
2017-12-17 19:07:18 +0100

    the end

--- scripts.js
+++ scripts.js
@@ -4,6 +4,16 @@ function Button(text) {
 
 Button.prototype = {
 	create: function(){
+		var self = this;
 		this.$element = $('<button>');
+		this.$element.text(this.text);
+		this.$element.click(function(){
+			alert(self.text);
+		$('body').append(this.$element);
+		})
 	}
 }
+
+var btn1 = new Button('Hello!');
+
+btn1.create();
